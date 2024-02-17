ba5ba8a2d963812ec810fcab491f6702bfc8d867
daapinn <123256229+daapinn@users.noreply.github.com>
2024-02-17 19:50:49 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,6 +23,10 @@ from streamlit_option_menu import OptionMenu
 # In[6]:
 
 @st.cache
+def load_data():
+    df_data_hour = pd.read_csv('hour.csv')
+    return df_data_hour
+
 df_data_hour = load_data()
 
 
